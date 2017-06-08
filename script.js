3f7b8ff21f4d06e89593a72f9a469e6b418d93a7
sh3466 <sh3466@tc.columbia.edu>
2017-06-08 16:08:54 +0200

    update

--- script.js
+++ script.js
@@ -2,16 +2,15 @@
 
 function save(event) {
 	alert("Saved!");
-	event.preventDefault();
+	event.stopPropagation();
 }
 
 // reset - WRITE alert, reset page elements ONCLICK, reset completion info ONCLICK, change state when nothing new vs new ONCHANGE
 
 function clear(event) {
-	event.preventDefault();
 	document.querySelector('#notes') = "reset";
-	
 }
+
 
 
 // linkImage - show image from address
