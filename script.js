2cf3a0f1012bf83a141a1c6ed9a1e1b03ec1af3f
sh3466 <sh3466@tc.columbia.edu>
2017-06-08 14:03:44 +0200

    updates

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // save - WRITE alert, store inputs (text, radio, checkboxes) ONCLICK, update completion info ONCLICK, change state when nothing new vs new ONCHANGE
+<script>
 
+function save(e) {
+	e.preventDefault();
+}
 
 // reset - WRITE alert, reset page elements ONCLICK, reset completion info ONCLICK, change state when nothing new vs new ONCHANGE
 
@@ -10,3 +14,5 @@
 
 // uploadImage - show image from upload
 // on
+
+</script>
