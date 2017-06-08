7c9114b2f1c3639655634007e224ae5fb4610ac0
sh3466 <sh3466@tc.columbia.edu>
2017-06-08 15:04:35 +0200

    update

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // save - WRITE alert, store inputs (text, radio, checkboxes) ONCLICK, update completion info ONCLICK, change state when nothing new vs new ONCHANGE
 
-function save(e) {
+function save() {
 	alert("Saved!");
 }
 
 // reset - WRITE alert, reset page elements ONCLICK, reset completion info ONCLICK, change state when nothing new vs new ONCHANGE
 
-function reset(e) {
+function clear() {
 	document.querySelector('#notes') = "reset";
 }
 
