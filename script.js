d660f5aac3fe089cb57893b71c058096bb417308
sh3466 <sh3466@tc.columbia.edu>
2017-06-09 09:36:56 +0200

    update

--- script.js
+++ script.js
@@ -2,31 +2,43 @@
 // listen for events - click, change, blur ...
 // write content to the page based on user action
 // manipulate html/css elements
-
 // variables to remember and recall states
+
 // small functions into larger functions
 
 
 // ! WRITE alert ONCLICK
-// ! "store" inputs (text, radio, checkboxes) ONCLICK === don't refresh
+
 function save(event) {
 	alert("Saved!");
 }
 
-// clear page elements ONCLICK but don't refresh
-function clearTextNB(event) {
+// RESET elements ONCLICK
+
+function resetTextNB(event) {
 	document.getElementById('notebookArea').value = "";
 }
-function clearTextR(event) {
+
+function resetTextR(event) {
+	var radioEls = document.querySelectorAll('#rating input[type="radio"]:checked');
+		for (var radio of radioEls){
+			radio.checked = false;
+		}
 	document.getElementById('reflectionArea').value = "";
 }
-function clearTextC(event) {
+
+function resetTextC(event) {
+	var checkboxCritique = document.querySelectorAll('#critiqueInputs input[type="checkbox"]:checked')
+		for (var checkbox of checkboxCritique) {
+			checkbox.checked = false;
+		}
 	document.getElementById('critiqueArea').value = "";
 }
 
-// linkImage - show image from address
-// onclick
+// DISPLAY image ONCLICK
 
-
-// uploadImage - show image from upload
-// on
+function displayImgC1(event) {
+	//alert("just checking");
+	var imgEl = document.getElementById('selection1img');
+	imgEl.src = document.getElementById('selection1URL');
+}
